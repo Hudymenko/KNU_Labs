b5185a30fc3f8d92331d7272abb8c994757f5a86
Hudymenko <120525194+Hudymenko@users.noreply.github.com>
2022-12-14 00:19:54 +0200

    Add files via upload

---
+++ Lab3/Lab3.R
@@ -0,0 +1,49 @@
+setwd("C:/Users/USER/Desktop/DataProcessingLabs/Lab3")
+data <- readRDS("hike_data.rds")
+
+data[1, "features"]
+
+get_type <- function (x) strsplit(x, split = ", ")[[1]][2]
+
+get_length <- function (x) {
+  miles <- strsplit(x, split = " ")[[1]][1]
+  miles <- as.numeric(miles)
+  type <- strsplit(x, split = " ")[[1]][3]
+  if (type == "one-way") {
+    miles <- miles * 2
+  } 
+  return(miles)
+}
+
+get_location <- function(x) strsplit(x, split = " -- ")[[1]][1]
+
+cht <- data.frame(
+  gain = as.numeric(data$gain),
+  highpoint = as.numeric(data$highpoint),
+  rating = as.numeric(data$rating),
+  trip = sapply(data$length, function(x) strsplit(x, split = ", ")[[1]][2]),
+  length_total = sapply(data$length, get_length),
+  location_general = sapply(data$location, get_location),
+  id = seq.int(nrow(data))
+)
+
+head(cht)
+
+# Question 1. How many routes have rating more than 4.9
+new_data <- subset(cht, rating > 4.9)
+nrow(new_data)
+
+# Question 2. How many routes are “Good for kids” (hint: you can use (unnest function)?
+new_data <- subset(unnest(data, cols = c("features")), features == "Good for kids")
+nrow(new_data)
+
+# Question 3. Which unique features can routes have?
+features <- unique(unnest(data, cols = c("features"))$features)
+features
+
+# Question 4. What is the most common rating of a route?
+tail(names(sort(table(cht$rating))), 1)
+
+# Question 5. Your own question and answer. How many routes have highpoint more than 10000?
+new_data <- subset(cht, highpoint > 10000)
+nrow(new_data)
